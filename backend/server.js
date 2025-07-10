df79b3d2a2fc8b8a34df79b541498c04af5265cc
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-07-10 15:09:54 +0530

    setting up the env file

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("It's working!");
 });
 
-app.listen(5001, ()=>{
-    console.log("Server is up and running on PORT:5001");
+app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
 });
